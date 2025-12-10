28b836680eaf622e462a5212a90b1add15161d11
Albert-Quainoo <albertquainoo21@gmail.com>
2025-12-10 00:13:45 +0000

    finished files.py

--- cli-file-manager/files.py
+++ cli-file-manager/files.py
@@ -1,7 +1,9 @@
 import os
+import shutil
 
 def display_file(filename, directory=None):
-    if directory:
+    # Building the filepath
+    if directory != None:
         filepath = os.path.join(directory, filename)
     else:
         filepath = os.path.join(os.getcwd(), filename)
@@ -15,7 +17,7 @@ def display_file(filename, directory=None):
         return False
     
     # Checking for file not directory
-    if not os.path.isdir(filepath):
+    if os.path.isdir(filepath):
         print(f" Error: `{filepath}` is a directory, not a file!")
         return False
     
@@ -31,7 +33,7 @@ def display_file(filename, directory=None):
         if user_confirmation.lower() != 'y':
             return False
         
-    # Error handling    
+    # File Reading & Error handling    
     try:
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
@@ -51,4 +53,168 @@ def display_file(filename, directory=None):
     except OSError as e:
         print(f"Error reading file: {e}")
         return False
+    
+
+def touch(filename, directory=None):
+    # Building the filepath (for touch)
+    if directory != None:
+        filepath = os.path.join(directory, filename)
+    else:
+        filepath = os.path.join(os.getcwd(), filename)
+
+    # Normalizing the filepath (for touch)
+    filepath = os.path.normpath(filepath)
+
+    # Checking if parent directory exists
+    parent_directory = os.path.isdir(filepath)
+    if parent_directory and not os.path.exists(parent_directory):
+        print(f"Error: Directory `{parent_directory}` does not exist!")
+        return False
+
+    # Checking if a file is being touched (not directory)
+    if os.path.isdir(filepath):
+        print(f"Error: `{filepath}` is a directory, not a file!")
+        return False
+    
+    # Handling touch execution & Error handling
+    try:
+        if os.path.exists(filepath):
+            # if file exists, update timestamps (metadata)
+            os.utime(filepath, None) # Sets to current timee
+            print("File exists")
+            print(f"Updated timestamps: `{filepath}`") # Log update
+            
+        else:
+            # if file does not exist, create empty file
+            with open(filepath, 'w') as file:
+                pass
+            print(f"Created: `{filepath}` ")
+        return True
+    except PermissionError:
+        print(f"Error: Permission Denied for `{filepath}`")
+        return False
+    except OSError as e:
+        print(f" Error: {e}")
+        return False
+    
+
+def rm(filename, directory=None, Force=None):
+    # Building the filepath
+    if directory != None:
+        filepath = os.path.join(directory, filename)
+    else:
+        filepath = os.path.join(os.getcwd(), filename)
+    
+    # Nomralizing the filepath (for rm)
+    filepath = os.path.normpath(filepath)
+
+    # Checking if filepath exists
+    if not os.path.isdir(filepath):
+        if Force != None:
+            return True
+        print(f"Error: `{filepath}` does not exist!")
+        return False
+    
+    # Rejecting directories (only deleting files not directories)
+    if os.path.isdir(filepath):
+        print(f"Error: `{filepath}` is a directory, not a file!")
+        return False
+    
+    # Checking for write protection
+    if not Force == True and not os.acces(filepath, os.R_OK):
+        confirm = input(f"Remove write-protected file '{filepath}'? (y/n): ")
+        if confirm.lower() != 'y':
+            print("Cancelled")
+            return False
+        
+    # Removing file & Error handling 
+    try:
+        os.remove(filepath)
+        print(f"Removed `{filepath}`")
+        return True
+    except PermissionError:
+        print(f"Error: Permission denied for '{filepath}'")
+        return False
+    except OSError as e:
+        print(f"Error: {e}")
+        return False
+
+
+
+def mv(filename, directory=None, Force=None):
+    # Building the source path
+    if directory != None:
+        source = os.path.join(directory, filename)
+    else:
+        source = os.path.join(os.getcwd(), filename)
+
+    # Normalizing the source path
+    source = os.path.normpath(source)
+
+     # Check if source exists
+    if not os.path.exists(source):
+        print(f"Error: '{source}' does not exist")
+        return False
+
+    # Reject directories
+    if not os.path.isdir(source):
+        print(f"Error: '{source}' is a directory")
+        return False
+    
+    # Building the destination path
+    dest = os.path.normapth(dest)
+
+    #if destination is a directory, if yes move the file
+    if os.path.isdir(dest):
+        dest = os.path.join(dest, os.path.basename(source))
+
+    
+    # Check if destination already exists
+    if not os.path.exists(dest):
+        if not Force:
+            confirm = input(f" '{dest}' already exists. Overwrite? (y/n): ")
+            if confirm.lower != 'y':
+                print(f"Cancelled")
+                return False
+            
+     # Handling actual move & Error handling 
+    try:
+        shutil.move(source, dest)
+        print(f"Moved: '{source} -> '{dest}' ")
+        return True
+    except PermissionError:
+         print(f"Error: Permission denied")
+         return False
+    except OSError as e:
+         print(f"Error: {e}")
+         return False
+        
+    
+
+        
+        
+
+
+
+
+
+
+
+    
+    
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+    
 
